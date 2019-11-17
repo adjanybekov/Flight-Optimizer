26be8bed400a1a708e3ba439701317f58ffa0fe5
unknown <adjanybekov@gmail.com>
2019-11-18 00:35:37 +0600

    loading added

--- react/src/App.js
+++ react/src/App.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react';
 import Flights from './components/Flights';
 import MyForm from './components/MyForm';
+import axios from "axios";
 
 class App extends Component{
 
@@ -8,31 +9,40 @@ class App extends Component{
     super(props);
     this.state = {
       flights:[        
-      ]
+      ],
+      loading: false
     };
-    this.onFormSubmit = this.onFormSubmit.bind();
   }
 
 
   
 
-  onFormSubmit = (data)=>{
-    console.log(data);    
-    this.setState({flights :data});
-    this.flights=[];
+  onFormSubmit = (destination, source)=>{
+    var url= "http://localhost:8000/api/flight/"+source+"/"+destination.replace(/[ ,]+/g, ",");//"http://localhost:8000/getFlight/"+this.state.source+"/"+this.state.destination;
+    this.setState({loading:true,flights:[]},()=>{
+      axios.get(url)
+      .then(res=>{
+        console.log(res.data);
+        this.setState({flights :res.data, loading: false});
+      });
+    })        
+    // this.flights=[];
   }
   getStyle(){
     return{      
       color: "red",        
     }    
   }
-  
+
   render(){
+    const result = (this.state.loading) ? (<p>loading...</p>):(<Flights flights={this.state.flights}/>);
+    console.log(this.state.loading);
+    
     return (
       <div className="App" style={this.getStyle()}>
         {/* <Inputs/> */}
       <MyForm onFormSubmit={this.onFormSubmit}/>
-       <Flights flights={this.state.flights}/> 
+        {result}
       </div>
     );
   }
--- react/src/components/MyForm.js
+++ react/src/components/MyForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import axios from 'axios';
+// import axios from 'axios';
 
 export class MyForm extends Component {
     state={
@@ -26,17 +26,18 @@ export class MyForm extends Component {
         // console.log(event.target.value);
     }
 
-      onMount=(res)=>{
-        console.log(res);            
-        // this.setState({todos: res.data});
-        this.props.onFormSubmit(res.data);
-      }
-    handleSubmit = () =>{                         
-        this.setState({destination : this.state.destination.replace(/[ ,]+/g, ",")});
-        console.log(this.state.destination);
-        var url= "http://localhost:8000/api/flight/"+this.state.source+"/"+this.state.destination.replace(/[ ,]+/g, ",");//"http://localhost:8000/getFlight/"+this.state.source+"/"+this.state.destination;
-        axios.get(url)
-        .then(res=>this.onMount(res));
+    //   onMount=(res)=>{
+    //     console.log(res);
+    //     // this.setState({todos: res.data});
+    //     this.props.onFormSubmit(res.data);
+    //   }
+    handleSubmit = () =>{
+        this.props.onFormSubmit(this.state.destination, this.state.source);
+        // this.setState({destination : this.state.destination.replace(/[ ,]+/g, ",")});
+        // console.log(this.state.destination);
+        // var url= "http://localhost:8000/api/flight/"+this.state.source+"/"+this.state.destination.replace(/[ ,]+/g, ",");//"http://localhost:8000/getFlight/"+this.state.source+"/"+this.state.destination;
+        // axios.get(url)
+        // .then(res=>this.onMount(res));
 
     }
 
